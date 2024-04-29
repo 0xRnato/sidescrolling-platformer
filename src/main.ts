5c4921435296c9071707a3bb330a3d0c2dce6746
Renato Neto <rnato.netoo@gmail.com>
2024-04-29 04:26:47 -0300

    ground tilemap

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,28 @@
 import { Game as MainGame } from './scenes/Game';
-import { AUTO, Game, Scale,Types } from "phaser";
+import { AUTO, Game, Scale, Types } from "phaser";
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Types.Core.GameConfig = {
-    type: AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scale: {
-        mode: Scale.FIT,
-        autoCenter: Scale.CENTER_BOTH
-    },
-    scene: [
-        MainGame
-    ]
+  type: AUTO,
+  width: 600,
+  height: 600,
+  parent: 'game-container',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 },
+      debug: false
+    }
+  },
+  // backgroundColor: '#028af8',
+  // scale: {
+  //     mode: Scale.FIT,
+  //     autoCenter: Scale.CENTER_BOTH
+  // },
+  scene: [
+    MainGame
+  ]
 };
 
 export default new Game(config);
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,30 +1,21 @@
-import { Scene } from 'phaser';
+import Phaser from 'phaser';
 
-export class Game extends Scene
-{
-    constructor ()
-    {
-        super('Game');
-    }
+export class Game extends Phaser.Scene {
+  constructor() {
+    super('game');
+  }
 
-    preload ()
-    {
-        this.load.setPath('assets');
-        
-        this.load.image('background', 'bg.png');
-        this.load.image('logo', 'logo.png');
-    }
+  preload() {
+    this.load.atlas('penguin', 'assets/penguin.png', 'assets/penguin.json');
+    this.load.image('tiles', 'assets/sheet.png');
+    this.load.tilemapTiledJSON('tilemap', 'assets/iceworld.json');
+  }
 
-    create ()
-    {
-        
-        this.add.image(512, 384, 'background');
-        this.add.image(512, 350, 'logo').setDepth(100);
-        this.add.text(512, 490, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
-        
-    }
+  create() {
+    const map = this.make.tilemap({ key: 'tilemap' });
+    const tileset = map.addTilesetImage('iceworld', 'tiles');
+    const ground = map.createLayer('ground', tileset);
+
+    this.cameras.main.scrollY = 300
+  }
 }
